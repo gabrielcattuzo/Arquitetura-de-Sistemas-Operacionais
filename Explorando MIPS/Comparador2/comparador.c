0de343448fd93aac6f754d597ccbde987108476a
Gabriel Cattuzo <gabrielcattuzo@gmail.com>
2026-08-29 13:23:44 -0300

    feature: comparador

--- Explorando MIPS/Comparador2/comparador.c
+++ Explorando MIPS/Comparador2/comparador.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int num1, num2, num3;
+    int num1, num2, num3, maior;
 
     printf("Este programa faz a leitura de tres numeros inteiros e determina qual o maior deles\n");
 
@@ -15,44 +15,56 @@ int main()
     printf("\nInsira o terceiro numero: ");
     scanf("%d", &num3);
 
-    if (num1 > num2)
+    maior = num1;
+
+    if (num2 > maior)
+    {
+        maior = num2;
+    }
+
+    if (num3 > maior)
+    {
+        maior = num3;
+    }
+
+    if (num1 == maior && num2 == maior && num3 == maior)
+
     {
 
-        if (num1 > num3)
-        {
+        printf("\nOs tres numeros empataram em %d", maior);
 
-            printf("\nO maior numero eh %d", num1);
+    }
 
-        }
-        else
+    else if (num1 == maior && num2 == maior)
 
-        {
+    {
 
-            printf("\nO maior numero eh %d", num3);
+        printf("\nO primeiro e o segundo numero empataram em %d", maior);
 
-        }
+    }
+
+    else if (num1 == maior && num3 == maior)
+
+    {
+
+        printf("\nO primeiro e o terceiro numero empataram em %d", maior);
+
+    }
+
+    else if (num2 == maior && num3 == maior)
+
+    {
+
+        printf("\nO segundo e o terceiro numero empataram em %d", maior);
+
     }
 
     else
 
     {
 
-        if (num2 > num3)
+        printf("\nO maior numero eh %d", maior);
 
-        {
-
-            printf("\nO maior numero eh %d", num2);
-
-        }
-
-        else
-
-        {
-
-            printf("\nO maior numero eh %d", num3);
-
-        }
-        
     }
 
     return 0;
